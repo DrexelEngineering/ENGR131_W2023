ac7c017835bb680e538b3ad7c5f4d09196153727
Josh Agar <jca92@drexel.edu>
2023-01-05 21:38:14 -0500

    build update

---
+++ _build/jupyter_execute/jupyterbook/week1/lecture1/1_3_Who_is_Prof_Agar.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Who is Professor Agar
+
+# In[1]:
+
+
+from IPython.display import HTML
+
+HTML('<iframe src="https://drexel0-my.sharepoint.com/personal/jca92_drexel_edu/_layouts/15/Doc.aspx?sourcedoc={c1ad3f06-7687-409e-9435-d493b5971a15}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="476px" height="288px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>')
+
+
+# [Direct Link]( https://drexel0-my.sharepoint.com/:p:/g/personal/jca92_drexel_edu/EQY_rcGHdp5AlDXUk7WXGhUBsrIOExSzdIS8-IiLFX63tg)
+
+# In[ ]:
+
+
+
+
