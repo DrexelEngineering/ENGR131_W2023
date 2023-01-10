745f87871bcbbeb43ddeb87f529bbad9ab32af15
Josh Agar <jca92@drexel.edu>
2023-01-09 23:34:52 -0500

    push with intro

---
+++ _build/html/_sources/jupyterbook/resources/resources.md
@@ -0,0 +1,3 @@
+# Resources
+
+1. {doc}`How to use peer programming             <./peer_programming>`
---
+++ _build/jupyter_execute/jupyterbook/resources/peer_programming.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Peer Programing
+# 
+# We find it very valuable to work collaboratively on code. Our JupyterHub provides an interface for peer programing. Here we provide instructions
+
+# 1. Make sure you are connected to the Drexel network or logged into the VPN
+# 2. Login using the shared credential provided to you. They will look like `Lab_X_Y`, or `Lecture_X_Y`
+# ![](./figs/1.png)
+# 3. Access the file that you would like to work on either through the website, creating a new file, or uploading it within the interface. 
+# 4. Click the share menu and select `share Jupyter server link`
+# ![](./figs/2.png)
+# 5. Copy the link by clicking `copy link` - Share this link with your team (note, if you use the link provided in your email you will have to navigate to the same file)
+# ![](./figs/3.png)
+# 
+# **If you have been provided with a token skip to step**
+# 
+# 6. Go to file and select `hub control panel`.
+# ![](./figs/4.png)
+# 
+# 7. Click on `token`
+# 
+# ![](./figs/5.png)
+# 
+# 8. Set a time when the token should expire and click create token
+# 
+# ![](./figs/6.png)
+# 
+# 9. Copy the token provided. You will only be able to see this once. 
+# 
+# ![](./figs/7.png)
+# 
+# 10. Share the token with your team
+# 
+# 11. Construct the URL by taking the part that was copied and adding the token to then end.
+# 
+# It should look something like `https://jupyterhub.coe.drexel.edu/user/XXXXX/lab/?token=YOURTOKENHERE`
